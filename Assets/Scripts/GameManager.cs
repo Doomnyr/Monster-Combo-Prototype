95f8af53ba89d1f01edf589471c6f81538ed8b3b
Doomnyr <38334591+Doomnyr@users.noreply.github.com>
2026-06-28 13:31:44 -0500

    fixed setting menu and load battle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,10 @@
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-
-    [SerializeField] private GameObject settings;
 
     void Awake()
     {
@@ -28,51 +24,5 @@ public class GameManager : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
-    }
-
-    public void LoadGame()
-    {
-        StartCoroutine(WaitAndLoad("CombatScene", 1f));
-    }
-
-        IEnumerator WaitAndLoad(string sceneName, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        SceneManager.LoadScene(sceneName);
-    }
-
-    void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        // Assign it from the new scene (adjust to your setup)
-        if (settings == null)
-            settings = GameObject.Find("SettingsPanel"); // or use a tag / cached type
-        if (settings != null)
-            settings.SetActive(false);
-    }   
-
-    public void OpenSettingPanel()
-    {
-        if (settings != null) settings.SetActive(true);
-    }
-
-    public void CloseSettingPanel()
-    {
-        if (settings != null) settings.SetActive(false);
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Quitting Game");
-        Application.Quit();
     }
 }
